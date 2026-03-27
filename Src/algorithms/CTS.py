3497b4a1e7be8519ce02276033f120c7959dd6bd
GwenaelleLCT <glecallet.ing2026@esaip.org>
2026-03-27 14:10:25 +0100

    amelioration du code CTS.py

--- Src/algorithms/CTS.py
+++ Src/algorithms/CTS.py
@@ -20,33 +20,37 @@ import numpy as np
 
 class CTS():
 
-    def __init__(self, arms=None, dimension_context=None): 
+    def __init__(self, arms=None, context_example=None): 
         
         self.ground_arms = arms
         self.arms_pool = self.ground_arms.copy()
         self.name = "CTS"
 
-        self.arms_payoff_vectors = {"cumulated_rewards" : np.zeros(len(self.ground_arms)),
-                                    "tries" : np.zeros(len(self.ground_arms))
+        nb_arms = len(self.ground_arms)
+        nb_dimensions = context_example.shape[1] - 1
+        self.arms_payoff_vectors = {"cumulated_rewards" : np.zeros(nb_arms),
+                                    "tries" : np.zeros(nb_arms),
+                                    "covariance_matrix" : np.zeros((nb_arms, nb_dimensions, nb_dimensions)),
+                                    "inverse_covariance_matrix" : np.zeros((nb_arms, nb_dimensions, nb_dimensions)),
+                                    "features_matrix" : np.zeros((nb_arms, nb_dimensions,)),
+                                    "theta_hat" : np.zeros((nb_arms, nb_dimensions,))
                                     }
         
+        self.arms_payoff_vectors["covariance_matrix"][:] = np.eye(nb_dimensions).astype(float)
+        self.arms_payoff_vectors["inverse_covariance_matrix"][:] = np.eye(nb_dimensions).astype(float)
+            
+        
         self.arm_chosen = None
+        # Paramètre de variance
+        self.v = 0.5
+
         self.threshold = 4
 
-        # Paramètre de variance
-        self.v = 0.01
-        # dimension du contexte 
-        self.dimension_context = dimension_context
-        # Initialisation des matrices A et vecteurs b pour chaque bras
-        self.A = np.array([np.identity(self.dimension_context) for arm in range(len(self.ground_arms))]) 
-        self.b = np.array([np.zeros(self.dimension_context) for arm in range(len(self.ground_arms))]) 
-        
         
         # -------------------------------------------------------------------
 
     def run(self, observed_value, user_context=None):
         
-        # Contexte
         self.current_context = user_context
 
         self.init_choice(observed_value)
@@ -68,25 +72,20 @@ class CTS():
     def choose_action(self):
         
         arm_pool_size = len(self.arms_pool['arm_id']) 
-        sampled_values = np.zeros(arm_pool_size) 
+        expected_payoff = np.zeros(arm_pool_size)
 
         i = 0
         for arm in self.arms_pool['arm_id']:
             arm_pos = self.ground_arms.index[self.ground_arms["arm_id"] == arm][0]
-            A_inv = np.linalg.inv(self.A[arm_pos])
+             
+            self.arms_payoff_vectors["theta_hat"][arm_pos] = np.random.multivariate_normal( np.dot(self.arms_payoff_vectors["inverse_covariance_matrix"][arm_pos], self.arms_payoff_vectors["features_matrix"][arm_pos]), \
+                                                       (self.v ** 2) * self.arms_payoff_vectors["inverse_covariance_matrix"][arm_pos])
             
-            X = A_inv @ self.b[arm_pos]
-            
-            Y = (self.v ** 2) * A_inv
-            
-            sampled_theta = np.random.multivariate_normal(X, Y)
-            
-            x = self.current_context
-            sampled_values[i] = sampled_theta @ x
+            expected_payoff[i] = np.dot(self.arms_payoff_vectors["theta_hat"][arm_pos].T, self.current_context)
             
             i += 1
             
-        arm_chosen_index = np.argmax(sampled_values) 
+        arm_chosen_index = np.argmax(expected_payoff) 
         arm_chosen = self.arms_pool["arm_id"][arm_chosen_index]
             
         return arm_chosen
@@ -107,16 +106,12 @@ class CTS():
     def update(self, observation):
 
         observed_reward = self.evaluate(observation)
+        self.arms_payoff_vectors["cumulated_rewards"][self.arm_chosen] += observed_reward
+        self.arms_payoff_vectors["tries"][self.arm_chosen] += 1
         
-        arm_pos = self.ground_arms.index[self.ground_arms["arm_id"] == self.arm_chosen][0]
-        x = self.current_context
-        
-        # Mise à jour de A et b pour le bras choisi
-        self.A[arm_pos] += np.outer(x, x)
-        self.b[arm_pos] += observed_reward * x
-        
-        self.arms_payoff_vectors["cumulated_rewards"][arm_pos] += observed_reward
-        self.arms_payoff_vectors["tries"][arm_pos] += 1
+        self.arms_payoff_vectors["covariance_matrix"][self.arm_chosen] += np.outer(self.current_context, self.current_context)
+        self.arms_payoff_vectors["inverse_covariance_matrix"][self.arm_chosen] = np.linalg.inv(self.arms_payoff_vectors["covariance_matrix"][self.arm_chosen])
+        self.arms_payoff_vectors["features_matrix"][self.arm_chosen] += observed_reward * self.current_context
                   
         
         # -------------------------------------------------------------------
